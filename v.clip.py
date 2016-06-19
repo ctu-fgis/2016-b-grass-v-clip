0248a4ee8e2d4b08667f3429aaf42ffba0737779
zofie-cimburova <zoficimburova@seznam.cz>
2016-06-19 12:19:45 +0200

    Input typology distinction (draft)

--- v.clip.py
+++ v.clip.py
@@ -46,7 +46,8 @@
 #%end
 
 # TODO - nepridava se vysledna mapa do seznamu vrstev
-# TODO - nemuze byt zaroven -d a -r
+# TODO - nemuze byt zaroven -d a -r => igonorovat -d pokud -r
+# TODO - chyby pri spusteni - nekdy, po opakovanem spusteni bez chyby
 
 # TODO - co z tohohle muzu smazat?
 from grass.script import run_command, message, percent, parser
@@ -64,56 +65,95 @@ def main():
     flag_dissolve = flg['d']
     flag_region   = flg['r']
     
-  
-    # ---- clip with dissolve ---- #
-    # TODO - Martin - dissolve without input column
-    if (flag_dissolve):
-        grass.message("Flag - dissolve")
+    # ======================================== #
+    # ========== INPUT MAP TOPOLOGY ========== #
+    # ======================================== #
+    lines_count = grass.vector_info_topo(input_map)['lines']
+    points_count = grass.vector_info_topo(input_map)['points']
+    areas_count = grass.vector_info_topo(input_map)['areas']
+    grass.message("there are {0} lines, {1} points and {2} areas".format(lines_count, points_count, areas_count))
+    
+    # only points
+    if (points_count > 0 and lines_count == 0 and areas_count == 0):
+        grass.message('only points')
+        # TODO v.select
+        
+        # ==================================== #
+        # ========== CLIP BY REGION ========== #
+        # ==================================== #
+        if (flag_region):
+            grass.message("Flag - region")
+            # TODO
+        
+        # ================================= #
+        # ========== NORMAL CLIP ========== #
+        # ================================= #
+        else:
+            grass.message("Normal clipping")
+        
+        
+        
+        
+    else:
+        # lines, areas, lines + areas
+        if (points_count == 0):
+            grass.message('lines or areas')
+        
+        # points + areas, points + lines, points + areas + lines
+        else:
+            grass.warning("Input map contains multiple geometry, only lines and areas will be clipped.")
+    
+        # ==================================== #
+        # ========== CLIP BY REGION ========== #
+        # ==================================== #
+        # TODO - disable clip layer option?
+        if (flag_region):
+            grass.message("Flag - region")
+            
+            # setup temporary file
+            temp_region_map = '%s_%s' % ("temp", str(os.getpid()))
+            
+            # create a map covering current computational region
+            grass.run_command('v.in.region', output = temp_region_map)
+            grass.message(temp_region_map)
+            
+            # perform clipping
+            clip(input_map, temp_region_map, output_map)
+            
+            # delete temporary file
+            grass.run_command('g.remove', flags='f', type='vector', name=temp_region_map)
+        
+        # ======================================== #
+        # ========== CLIP WITH DISSOLVE ========== #
+        # ======================================== #
+        # TODO - Martin - dissolve without input column
+        elif (flag_dissolve):
+            grass.message("Flag - dissolve")
 
-        # setup temporary file
-        temp_clip_map = '%s_%s' % ("temp", str(os.getpid()))
+            # setup temporary file
+            temp_clip_map = '%s_%s' % ("temp", str(os.getpid()))
+            
+            # dissolve clip_map
+            grass.run_command('v.dissolve', input = clip_map, output = temp_clip_map)
+            grass.message(temp_clip_map)
+            
+            # perform clipping
+            clip(input_map, temp_clip_map, output_map)
+            
+            # delete temporary file
+            grass.run_command('g.remove', flags='f', type='vector', name=temp_clip_map)
         
-        # dissolve clip_map
-        grass.run_command('v.dissolve', input = clip_map, output = temp_clip_map)
-        grass.message(temp_clip_map)
-        
-        # perform clipping
-        clip(input_map, temp_clip_map, output_map)
-        
-        # delete temporary file
-        grass.run_command('g.remove', flags='f', type='vector', name=temp_clip_map)
-    
-    
-     # ---- clip by region ---- #
-     # TODO - disable clip layer option?
-    if (flag_region):
-        grass.message("Flag - region")
-        
-        # setup temporary file
-        temp_region_map = '%s_%s' % ("temp", str(os.getpid()))
-        
-        # create a map covering current computational region
-        grass.run_command('v.in.region', output = temp_region_map)
-        grass.message(temp_region_map)
-        
-        # perform clipping
-        clip(input_map, temp_region_map, output_map)
-        
-        # delete temporary file
-        grass.run_command('g.remove', flags='f', type='vector', name=temp_region_map)
-       
-    
-    
-    # ---- dissolve clippingn map ---- #
-    else: 
-        grass.message("No flag")
+        # ================================= #
+        # ========== NORMAL CLIP ========== #
+        # ================================= #
+        else: 
+            grass.message("No flag")
 
-        # perform clippings
-        clip(input_map, clip_map, output_map)
-    
-        # write cmd history:
-        grass.vector_history(output_map)
-    
+            # perform clippings
+            clip(input_map, clip_map, output_map)
+        
+        
+        
 def clip(input_data, clip_data, out_data):
     try:
         grass.message("before clipping")
